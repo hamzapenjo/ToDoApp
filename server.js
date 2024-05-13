13c2d13e043203211419172be12de4ee87be263b
Hamza <penjohamza193@gmail.com>
2024-05-13 11:31:18 +0200

    podesavanje okruzenja, povezivanje sa bazom

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+mongoose.connect('mongodb://localhost:27017/walterdb')
+const db = mongoose.connection
+db.on('error', (err) =>{
+    console.log(err)
+})
+db.once('open', () =>{
+    console.log('Povezan')
+})
+
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, ()=>{
+    console.log('Server funkcioniral na portu', PORT)
+})
